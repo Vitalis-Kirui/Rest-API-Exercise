71e7f302a9f992280c55c3bbb91db8a08625a417
Vitalis Kirui <vitaliskirui1@gmail.com>
2022-10-03 17:02:08 +0300

    moving views from project to app. Correcting a bug

--- product_api/views.py
+++ product_api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from product_api.models import Item, Category
+from product_api.serializers import ItemSerializer, CategorySerializer
 
-# Create your views here.
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    
+class ItemViewSet(viewsets.ModelViewSet):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer

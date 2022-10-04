89aff8cdfc957e7b98619a233fa718c4f985db6f
Vitalis Kirui <vitaliskirui1@gmail.com>
2022-10-04 20:32:04 +0300

    making changes in models

--- product_api/models.py
+++ product_api/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=30)
 
     def __str__(self):
         return self.name
 
 
 class Item(models.Model):
-    item_name = models.CharField(max_length=100)
+    item_name = models.CharField(max_length=50)
     price = models.FloatField(default=0)
     on_discount = models.BooleanField(default=False)
     discount_price = models.FloatField(blank=True, null=True)
